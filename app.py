4cc506e8dd515067018376c0c8f0e5a9406180a4
R4639 <R4639@lab.aca>
2019-09-05 12:22:34 -0500

    Usando sessionS

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import bottle
+from beaker.middleware import SessionMiddleware
+from configs.session import session_options
 
 app = bottle.app()
+app_session = SessionMiddleware(app, session_options)
 
 @app.route('/', method='GET')
 def index():
@@ -15,7 +21,7 @@ def send_static(filename):
 
 if __name__ == '__main__':
     bottle.run(
-        app=app, 
+        app=app_session, 
         host='localhost', 
         port=3000, 
         debug=True,
---
+++ configs/session.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+session_options = {
+  'session.type': 'file',
+  'session.cookie_expires': 6000,
+  'session.data_dir': './data',
+  'session.auto': True
+}
