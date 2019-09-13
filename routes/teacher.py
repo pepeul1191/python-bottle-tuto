3f0ecd4e3715f15756f5d97296b821e152a26f90
Pepe Valdivia <jvaldivia@softweb.pe>
2019-09-13 00:21:40 -0500

    Query param y path param

--- routes/teacher.py
+++ routes/teacher.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import json
-from bottle import Bottle, HTTPResponse
+from bottle import Bottle, HTTPResponse, request
 from configs.middlewares import headers
 from helpers.teacher_list import teachers
 
@@ -15,4 +15,24 @@ def list():
     return HTTPResponse(
       status=200, 
       body=json.dumps(teachers)
+    )
+
+@app.route('/get/<id>', method='GET')
+@headers
+# @session_false
+def get(id):
+    id = int(id)
+    return HTTPResponse(
+      status=200, 
+      body=json.dumps(teachers[id])
+    )
+
+@app.route('/search', method='GET')
+@headers
+# @session_false
+def search():
+    id = int(request.query.id)
+    return HTTPResponse(
+      status=200, 
+      body=json.dumps(teachers[id])
     )
